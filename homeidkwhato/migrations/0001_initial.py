178a259d5aaf89b5a4f3fb30fe87cdcbc46c480e
Crowdbotics <team@crowdbotics.com>
2020-08-31 08:08:03 +0000

    Update models

--- homeidkwhato/migrations/0001_initial.py
+++ homeidkwhato/migrations/0001_initial.py
@@ -7,15 +7,22 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Hesoyam',
+            name="Hesoyam",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('enmaefiles', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("enmaefiles", models.BigIntegerField()),
             ],
         ),
     ]
--- hwahawwaiapp/migrations/0001_initial.py
+++ hwahawwaiapp/migrations/0001_initial.py
@@ -7,15 +7,22 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='UDObshiyttah',
+            name="UDObshiyttah",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('fielnameismst', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("fielnameismst", models.BigIntegerField()),
             ],
         ),
     ]
